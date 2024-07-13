bda229bf016e5326e964476b418597007b79a2b6
ashikeelai13 <learnwithashik3@gmail.com>
2024-07-14 01:27:38 +0600

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,42 +1,44 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value})
-  }
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Don't refress the page
     try {
-      setLoading(true);
-      setError(false)
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-      })
-      const data = await res.json()
-      setLoading(false);
-      if(data.success === false) {
-        setError(true)
-        return
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
       }
-      navigate('/')
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error));
     }
-    catch (error) {
-      setLoading(false)
-      setError(true)
-    }
-  }
-  
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -55,8 +57,11 @@ export default function SignIn() {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85">
-          {loading ? 'Loading...' : 'Sign In'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -65,7 +70,9 @@ export default function SignIn() {
           <span className="text-blue-500 ">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Somthing went wrong"}</p>
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Somthing went wrong" : ""}
+      </p>
     </div>
   );
 }
